021e8a342d58d530186b509fef9b24caf3ed8adc
+mitchellengetich <+mitchellngetich24@gmail.com>
2025-06-16 02:59:34 +0300

    add routing

--- server/app.py
+++ server/app.py
@@ -6,3 +6,38 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)  # prints to terminal
+    return param  # displays in browser
+
+
+@app.route('/count/<int:param>')
+def count(param):
+    numbers = "\n".join(str(n) for n in range(param)) + "\n"
+    return numbers
+
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    result = None
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return "Invalid operation", 400
+
+    return str(result)
